7d1b11a6ea575f9d091e9db70772ea1983db4bd9
JKearnsl <boyarshinovn@gmail.com>
2024-01-20 15:59:53 +0300

    Добавлено уравнение прямой

--- src/compmath/utils/func.py
+++ src/compmath/utils/func.py
@@ -49,3 +49,23 @@ def tangent(fx: Callable[[float | int], float], x0: float | int) -> Callable[[fl
     """
 
     return lambda x: fx(x0) + derivative(fx)(x0) * (x - x0)
+
+
+def line_between_points(
+        x1: float | int,
+        y1: float | int,
+        x2: float | int,
+        y2: float | int
+) -> Callable[[float | int], float]:
+    """
+     Уравнение прямой в форме y = mx + b
+
+    :param x1:
+    :param y1:
+    :param x2:
+    :param y2:
+    :return:
+    """
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    return lambda x: m * x + b
