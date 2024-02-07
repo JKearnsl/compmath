d267425a544ffb9578dd0b382738ca28bab67813
JKearnsl <boyarshinovn@gmail.com>
2024-02-07 22:38:20 +0300

    Реализованы макеты методов СЛАУ

--- src/compmath/models/slat/sim.py
+++ src/compmath/models/slat/sim.py
@@ -8,5 +8,25 @@ class SIModel(BaseSLATModel):
 
         self._title = "Метод простых итераций"
 
-    def calc(self) -> None:
-        ...
+    def calc(self):
+        k = 0
+        n = len(self.a())
+        a_matrix = self.a()
+        b_vector = self.b()
+        x = self.x0.copy()
+
+        while True:
+            k += 1
+            x_prev = x.copy()
+            for i in range(n):
+                s = sum(a_matrix[i][j] * x[j] for j in range(n) if j != i)
+                x[i] = (b_vector[i] - s) / a_matrix[i][i]
+
+            # Оценка точности
+            delta = max(abs(x[i] - x_prev[i]) for i in range(n))
+
+            if delta <= self.eps or k >= self.iters_limit:
+                break
+
+        print(x, delta, k)
+        return x, delta, k
--- src/compmath/models/slat/zm.py
+++ src/compmath/models/slat/zm.py
@@ -9,5 +9,26 @@ class ZModel(BaseSLATModel):
         self._title = "Метод Зейделя"
         self._description = "Метод Зейделя - модификация метода простых итераций..."
 
-    def calc(self) -> None:
-        ...
+    def calc(self):
+        k = 0
+        n = len(self.a())
+        a_matrix = self.a()
+        b_vector = self.b()
+        x = self.x0.copy()
+
+        while True:
+            k += 1
+            x_prev = x.copy()
+            for i in range(n):
+                s1 = sum(a_matrix[i][j] * x[j] for j in range(i))
+                s2 = sum(a_matrix[i][j] * x_prev[j] for j in range(i + 1, n))
+                x[i] = (b_vector[i] - s1 - s2) / a_matrix[i][i]
+
+            # Оценка точности
+            delta = max(abs(x[i] - x_prev[i]) for i in range(n))
+
+            if delta <= self.eps or k > self.iters_limit:
+                break
+
+        print(x, delta, k)
+        return x, delta, k
