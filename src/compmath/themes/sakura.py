d90f4f86185ec182a79e4a60f6ae468c8a0bcc3f
JKearnsl <boyarshinovn@gmail.com>
2024-02-09 22:06:19 +0300

    Обновлен цвет первого фона; Изменены цвета text_primary -> text_tertiary

--- src/compmath/themes/sakura.py
+++ src/compmath/themes/sakura.py
@@ -8,13 +8,13 @@ class SakuraTheme(BaseTheme):
     __version__ = "0.0.1"
     __description__ = "Тема в стиле Сакура"
 
-    first_background: str = "#FFAEBD"
+    first_background: str = "#FFF8F7"
     second_background: str = "#FFE0E6"
     third_background: str = "#FFFFFF"
     primary: str = "#bf848f"
     hover: str = "#E0C6CB"
     text_header: str = "#c43f59"
-    text_primary: str = "#010000"
+    text_primary: str = "#ffffff"
     text_secondary: str = "#909fa6"
-    text_tertiary: str = "#ffffff"
+    text_tertiary: str = "#010000"
 # #b476aeff  cool color
