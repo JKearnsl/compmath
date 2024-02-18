7b326a72f3c227ddf43dd5605cf896a69678452c
JKearnsl <boyarshinovn@gmail.com>
2024-02-18 19:54:08 +0300

    bugfix

--- src/compmath/views/widgets/graphic.py
+++ src/compmath/views/widgets/graphic.py
@@ -210,7 +210,8 @@ class Graphic(QWidget):
 
     def add_plot(self, plot_items: list[PlotDataItem]):
         self._plots.append(plot_items)
-        self.graphic_slider.setEnabled(True)
+        if self.graphic_slider.isVisible():
+            self.graphic_slider.setEnabled(True)
         self.graphic_slider.setMaximum(len(self._plots) - 1)
         self.graphic_slider.setValue(len(self._plots) - 1)
         self.set_plot(len(self._plots) - 1)
