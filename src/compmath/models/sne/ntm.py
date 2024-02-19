b83d1d9382a5499fd454fda8d6bf12ddb51c2e88
JKearnsl <boyarshinovn@gmail.com>
2024-02-19 18:36:38 +0300

    Реализован метод Ньютона

--- src/compmath/models/sne/ntm.py
+++ src/compmath/models/sne/ntm.py
@@ -1,4 +1,8 @@
+from sympy import diff
+import numpy as np
+
 from compmath.models.sne.base import BaseSNEModel, TableRow
+from compmath.utils.func import solve_rel_var, make_callable
 
 
 class NTModel(BaseSNEModel):
@@ -11,34 +15,63 @@ class NTModel(BaseSNEModel):
 
     def calc(self):
         self.table.clear()
+        self.solve_log.clear()
+
+        if len(self.equations) != 2:
+            self.validation_error("Должно быть два уравнения")
+            return
+
+        func_str_1 = self.equations[0]
+        func_str_2 = self.equations[1]
+
+        self.solve_log.append(f"Уравнение 1: {func_str_1}")
+        self.solve_log.append(f"Уравнение 2: {func_str_2}")
+
+        # Проверка итерационной сходимости
+
+        if not self.is_converges(func_str_1, func_str_2):
+            self.notify_observers()
+            self.validation_error("Не выполнено условие сходимости")
+            return
+
+        # Решение
+
+        # Матрица Якоби
+        w = [
+            [diff(func_str_1, "x"), diff(func_str_1, "y")],
+            [diff(func_str_2, "x"), diff(func_str_2, "y")]
+        ]
+        x_vector = np.array(self.initial_guess, dtype=float)
+
+        self.solve_log.append(f"W(x, y) = {w}")
+
+        for i, row in enumerate(w):
+            for j, el in enumerate(row):
+                w[i][j] = make_callable(el)
 
         k = 0
-        n = len(self.a())
-        a_matrix = self.a()
-        b_vector = self.b()
-        x = self.x0.copy()
-        delta = 0
-
-        while True:
+        delta = 2 * self.eps
+        while delta > self.eps and k < self._iters_limit:
             k += 1
-            x_prev = x.copy()
-            for i in range(n):
-                s1 = sum(a_matrix[i][j] * x[j] for j in range(i))
-                s2 = sum(a_matrix[i][j] * x_prev[j] for j in range(i + 1, n))
-                x[i] = (b_vector[i] - s1 - s2) / a_matrix[i][i]
 
-            # Оценка точности
-            delta = max(abs(x[i] - x_prev[i]) for i in range(n))
+            # Вектор поправок delta_x_vector = - W(x_vector_1, x_vector_2)^-1 * F(x_vector_1, x_vector_2)
+            w_matrix = np.array([
+                [w[0][0](*x_vector), w[0][1](*x_vector)],
+                [w[1][0](*x_vector), w[1][1](*x_vector)]
+            ])
 
-            self.table.append(
-                TableRow(
-                    iter_num=k,
-                    vector=x.copy(),
-                    delta=delta
-                )
-            )
+            delta_x = -np.dot(np.linalg.inv(w_matrix), [
+                make_callable(func_str_1)(*x_vector),
+                make_callable(func_str_2)(*x_vector)
+            ])
 
-            if delta <= self.eps or k > self.iters_limit:
-                break
+            # Уточнение решения
+            x_vector = delta_x + x_vector
 
-        self.was_calculated()
+            # Расчет оценки достаточной точности
+            delta = np.max(np.abs(delta_x))
+
+            self.table.append(TableRow(k, x_vector, delta))
+
+        self.solve_log.append(f"\nРешение: {x_vector}")
+        self.notify_observers()
