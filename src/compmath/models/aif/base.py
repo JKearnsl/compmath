38eecfbd7a14619afb24d1b6e772e1c924578105
JKearnsl <boyarshinovn@gmail.com>
2024-02-25 17:15:40 +0300

    Обновлены типы поля results

--- src/compmath/models/aif/base.py
+++ src/compmath/models/aif/base.py
@@ -20,7 +20,7 @@ class BaseAIFModel(BaseGraphicModel):
         self._title = "None"
         self._description = "None"
         self._points: list[tuple[float, float]] = []
-        self.results: list[tuple[Graphic, list[str]]] = []
+        self.results: list[tuple[Graphic, list[str], tuple[float, float]]] = []
 
     @property
     def title(self) -> str:
