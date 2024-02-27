d44a69e42e4294afa2f41dc81b412e7f82ff0951
JKearnsl <boyarshinovn@gmail.com>
2024-02-27 21:04:30 +0300

    Добавлена тема в стиле кофе

---
+++ src/compmath/themes/coffee.py
@@ -0,0 +1,20 @@
+from compmath.themes.base import BaseTheme
+
+
+class CoffeTheme(BaseTheme):
+
+    __title__ = "Coffee"
+    __uuid__ = "00000000-0000-0000-0000-000000000003"
+    __author__ = "JKearnsl"
+    __version__ = "0.0.1"
+    __description__ = "Тема в стиле Кофе"
+
+    first_background: str = "#ece0d1"
+    second_background: str = "#dbc1ac"
+    third_background: str = "#DED0B6"
+    primary: str = "#967259"
+    hover: str = "#B2A59B"
+    text_header: str = "#634832"
+    text_primary: str = "#38220f"
+    text_secondary: str = "#70645a"
+    text_tertiary: str = "#38220f"
