fdc1e7a562b15ca34f99210c5ba76ea215d9718e
JKearnsl <boyarshinovn@gmail.com>
2024-03-07 15:37:24 +0300

    Реализован элемент преобразованной матрицы;

--- src/compmath/models/slat/gm.py
+++ src/compmath/models/slat/gm.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 
 from compmath.models.slat.base import BaseSLATModel
-from compmath.utils.func import gauss_calc
+from compmath.utils.func import gauss_calc, is_diagonal_dominance
 
 
 class GModel(BaseSLATModel):
@@ -26,8 +26,6 @@ class GModel(BaseSLATModel):
             log_original.append(
                 "\t".join(str(cell) for cell in row) + "\t|   " + str(b_vector[i])
             )
-        log_original.append("\nИсходный вектор свободных членов\n")
-        log_original.append("\t".join(str(cell) for cell in b_vector))
 
         log_original.append("\nПрямой ход метода Гаусса\n")
         original_gauss_vector = gauss_calc(a_matrix, b_vector, n)
@@ -43,8 +41,51 @@ class GModel(BaseSLATModel):
         self.results.append((log_original, [], "Исходная матрица"))
 
         # Преобразованная матрица
-        log_transformed = ["\nПреобразованная матрица\n"]
-        ...
+        log_transformed = ["\nИсходная матрица\n"]
+        for i, row in enumerate(a_matrix):
+            log_transformed.append(
+                "\t".join(str(cell) for cell in row) + "\t|   " + str(b_vector[i])
+            )
+        log_transformed.append("")
+
+        for i, row in enumerate(a_matrix):
+            sum_row = sum(abs(cell) for j, cell in enumerate(row) if j != i)
+            log_transformed.append(
+               f"sum({", ".join(f'|{cell}|' for j, cell in enumerate(row) if j != i)}) = "
+               f"{sum_row}   {'>' if sum_row > abs(row[i]) else '<='}   |{row[i]}|"
+               f"\t[{'-' if sum_row > abs(row[i]) else '+'}]"
+            )
+        log_transformed.append("")
+
+        if is_diagonal_dominance(a_matrix):
+            log_transformed.append("Матрица обладает диагональным преобладанием")
+            log_transformed.append("Необходимости в элементарных преобразованиях исходной матрицы нет")
+        else:
+            log_transformed.append("Матрица не обладает диагональным преобладанием")
+            log_transformed.append("Необходимо преобразовать исходную матрицу")
+
+            log_transformed.append("\nПреобразованная матрица\n")
+            transformed_a_matrix, transformed_b_vector, log = self.transform_matrix()
+            log_transformed.append("Шаги:")
+            log_transformed.extend(log)
+            log_transformed.append("")
+            for i, row in enumerate(transformed_a_matrix):
+                log_transformed.append(
+                    "\t".join(str(round(cell, 2)) for cell in row) + "\t|   " + str(round(transformed_b_vector[i], 2))
+                )
+
+            log_transformed.append("\nРешение методом Гаусса\n")
+
+            transformed_gauss_vector = gauss_calc(transformed_a_matrix, transformed_b_vector, n)
+            log_transformed.append("\n".join(str(cell) for cell in transformed_gauss_vector[0]))
+
+            log_transformed.append("\nВектор невязок\n")
+            log_transformed.append("\n".join(str(round(cell, 2)) for cell in transformed_gauss_vector[1]))
+
+            log_transformed.append("\nТреугольная матрица\n")
+            for row in transformed_gauss_vector[2]:
+                log_transformed.append("\t".join(str(round(cell, 2)) for cell in row))
+
         self.results.append((log_transformed, [], "Преобразованная матрица"))
 
         # Нормализованная матрица
