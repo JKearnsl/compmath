2557ee251e2fbcba9ef885b84350aebedd70df46
JKearnsl <boyarshinovn@gmail.com>
2024-03-07 11:59:08 +0300

    Инициализирована модель метода Гауса;

---
+++ src/compmath/models/slat/gm.py
@@ -0,0 +1,68 @@
+from copy import deepcopy
+
+from compmath.models.slat.base import BaseSLATModel
+from compmath.utils.func import gauss_calc
+
+
+class GModel(BaseSLATModel):
+
+    def __init__(self):
+        super().__init__()
+
+        self._title = "Метод Гаусса"
+        self._description = "Метод Гаусса - метод решения системы линейных уравнений..."
+
+    def calc(self):
+        self.results.clear()
+
+        n = len(self.a())
+        a_matrix = deepcopy(self.a())
+        b_vector = deepcopy(self.b())
+        x = deepcopy(self.x0)
+        delta = 0
+
+        # Исходная матрица
+        log_original = ["\nИсходная матрица\n"]
+        for row in a_matrix:
+            log_original.append("\t".join(str(cell) for cell in row))
+        log_original.append("\nИсходный вектор свободных членов\n")
+        log_original.append("\t".join(str(cell) for cell in b_vector))
+
+        log_original.append("\nПрямой ход метода Гаусса\n")
+        original_gauss_vector = gauss_calc(a_matrix, b_vector, n)
+        log_original.append("\n".join(str(cell) for cell in original_gauss_vector[0]))
+
+        log_original.append("\n".join(str(cell) for cell in original_gauss_vector[1]))
+
+        for row in original_gauss_vector[2]:
+            log_original.append("\t".join(str(round(cell, 2)) for cell in row))
+
+        self.results.append((log_original, [], "Исходная матрица"))
+
+        # Преобразованная матрица
+        log_transformed = ["\nПреобразованная матрица\n"]
+        ...
+        self.results.append((log_transformed, [], "Преобразованная матрица"))
+
+        # Нормализованная матрица
+        log_normalized = ["\nНормализованная матрица\n"]
+        normal_a_matrix, normal_b_vector = self.normal_matrix()
+        for row in normal_a_matrix:
+            log_normalized.append("\t".join(str(round(cell, 2)) for cell in row))
+
+        log_normalized.append("\nРешение методом Гаусса\n")
+        normal_gauss_vector = gauss_calc(normal_a_matrix, normal_b_vector, n)
+        log_normalized.append("\n".join(str(cell) for cell in normal_gauss_vector[0]))
+
+
+        log_normalized.append("\t".join(str(round(cell, 2)) for cell in normal_gauss_vector[1]))
+
+        for row in normal_gauss_vector[2]:
+            log_normalized.append("\t".join(str(round(cell, 2)) for cell in row))
+
+        self.results.append((log_normalized, [], "Нормализованная матрица"))
+
+
+
+        self.notify_observers()
+
