0ec5e0826d9e0c4f8e375f8d03974a5f45d351d8
JKearnsl <boyarshinovn@gmail.com>
2024-03-07 23:43:25 +0300

    Реализована фабрика API; Добавлен AIFClient;

---
+++ src/compmath/api/factory.py
@@ -0,0 +1,12 @@
+import urllib.parse as urllib
+
+from compmath.api.aif import AIFClient
+
+
+class APIFactory:
+    def __init__(self, host: str, port: int, scheme: str = "http", path: str = "api"):
+        self._base_url = urllib.urljoin(f"{scheme}://{host}:{port}", path)
+        print(self._base_url)
+
+    def create_aif(self) -> AIFClient:
+        return AIFClient(self._base_url)
