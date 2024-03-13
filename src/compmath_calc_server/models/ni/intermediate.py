ca6ffc081caf9a0f3ce9c10a1d0a5250986e2d9c
JKearnsl <boyarshinovn@gmail.com>
2024-03-13 14:24:54 +0300

    Улучшены методы вычисления длины поверхности и объема тела

--- src/compmath_calc_server/models/ni/intermediate.py
+++ src/compmath_calc_server/models/ni/intermediate.py
@@ -1,6 +1,8 @@
-from sympy import integrate, sympify, diff, sqrt, pi
+from sympy import integrate, sympify, pi
 
+from compmath_calc_server.utils.func import arc_length
 from compmath_calc_server.exceptions import BadRequest
+from compmath_calc_server.models import GraphicBuilder
 from compmath_calc_server.models.ni.dto import InputNInterModel, OutputNInterModel
 from compmath_calc_server.utils.func import surface_area
 
@@ -13,20 +15,22 @@ def calc(data: InputNInterModel) -> OutputNInterModel:
     y_limits = data.y_limits
 
     fx = sympify(fx_str)
-    fx_prime = diff(fx, 'x')
 
     if a > b:
         raise BadRequest("Левая граница интервала не может быть больше правой")
 
     reference_result = integrate(fx, ('x', a, b)).evalf()
     surface_area_value = surface_area(fx_str, a, b, 'x')
-    arc_length = integrate(sqrt(1 + fx_prime ** 2), ('x', a, b)).evalf()
-    volume = pi * integrate(fx ** 2, ('x', a, b)).evalf()
+    volume = (pi * integrate(fx ** 2, ('x', a, b))).evalf()
+    arc_length_value = arc_length(fx_str, a, b, 'x')
+
+    # graphic = GraphicBuilder(x_limits, y_limits)
+    # graphic.add_surface(fx, (a, b))
 
     return OutputNInterModel(
         graphic_items=[],
         reference_result=reference_result,
         surface_area=surface_area_value,
         volume=volume,
-        arc_length=arc_length
+        arc_length=arc_length_value
     )
