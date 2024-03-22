da9c16607d6d113325aa7d849fa57f2f3ab2b08f
JKearnsl <boyarshinovn@gmail.com>
2024-03-23 00:46:21 +0300

    transfer

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -2,30 +2,56 @@
 
 set -e
 
+# Variables
+SCRIPT_DIR=$(dirname "$0")
+
 PROGRAM_NAME='CompMath'
 PROGRAM_NAME_LOWER=$(echo "$PROGRAM_NAME" | tr '[:upper:]' '[:lower:]')
-PROGRAM_PATH=../src
+PROGRAM_PATH=$SCRIPT_DIR/../../src
+CONFIG_PATH=$SCRIPT_DIR/../../config.ini
+EXECUTABLE_PATH="${SCRIPT_DIR}/${PROGRAM_NAME_LOWER}.sh"
+UNINSTALL_SCRIPT_PATH="${SCRIPT_DIR}/uninstall.sh"
 PROGRAM_ASSET_PATH=$PROGRAM_PATH/$PROGRAM_NAME_LOWER/assets
-DESKTOP_FILE_PATH=./linux/.desktop
+DESKTOP_FILE_PATH=$SCRIPT_DIR/.desktop
 PYTHON_VERSION=3.12
 
 INSTALL_PATH=/opt/$PROGRAM_NAME_LOWER
 ICON_PATH=/usr/share/icons/hicolor
 
+# Color map
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BOLD=$(tput bold)
+ENDCOLOR=$(tput sgr0)
 
-echo 'Установка программы '$PROGRAM_NAME
+log() {
+  if [ "$1" = "info" ]; then
+    echo "$BOLD$GREEN$2$ENDCOLOR"
+  elif [ "$1" = "error" ]; then
+    echo "${BOLD}${RED}Ошибка: $2${ENDCOLOR}" >&2
+  elif [ "$1" = "warning" ]; then
+    echo "$BOLD$YELLOW$2$ENDCOLOR"
+  fi
+}
+
+trap 'echo "$RED$BOLDВозникла ошибка при установке программы $PROGRAM_NAME$ENDCOLOR"' EXIT
+
+
+
+log info 'Установка программы '$PROGRAM_NAME
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Ошибка: этот скрипт должен быть запущен с правами суперпользователя." >&2
+  log error "Этот скрипт должен быть запущен с правами суперпользователя."
   exit 1
 fi
 
 # Check if python3.12 is installed
 if ! [ -x "$(command -v python$PYTHON_VERSION)" ]; then
-  echo 'Внимание: python'$PYTHON_VERSION' не установлен.'
+  log warning 'Внимание: python'$PYTHON_VERSION' не установлен.'
 
   # Install python$PYTHON_VERSION
-  echo 'Установка python'$PYTHON_VERSION'...'
+  log info 'Установка python'$PYTHON_VERSION'...'
   if [ -x "$(command -v apt)" ]; then
     apt install python$PYTHON_VERSION
   elif [ -x "$(command -v pacman)" ]; then
@@ -67,8 +93,8 @@ if ! [ -x "$(command -v python$PYTHON_VERSION)" ]; then
   elif [ -x "$(command -v kcp)" ]; then
     kcp -i python$PYTHON_VERSION
   else
-    echo 'Ошибка: не удалось установить python'$PYTHON_VERSION >&2
-    echo 'Пожалуйста, установите python'$PYTHON_VERSION' вручную и повторите попытку' >&2
+    log error "Не удалось установить python$PYTHON_VERSION
+    Пожалуйста, установите python$PYTHON_VERSION вручную и повторите попытку"
     exit 1
   fi
 fi
@@ -83,28 +109,29 @@ cp "$DESKTOP_FILE_PATH" /usr/share/applications/"$PROGRAM_NAME_LOWER".desktop
 update-desktop-database /usr/share/applications
 
 # Install app
-echo 'Установка приложения...'
+log info 'Установка приложения...'
 
-cp -r $PROGRAM_PATH "$INSTALL_PATH"/
+cp -r "$PROGRAM_PATH" "$INSTALL_PATH"/
+
 
 # Create config file
 getent passwd | while IFS=: read -r username _ uid _ _ home _; do
   if [ "$uid" -ge 1000 ] && [ -d "$home" ]; then
     mkdir -p "$home/.$PROGRAM_NAME_LOWER"
-    cp ../config.ini "$home/.$PROGRAM_NAME_LOWER/config.ini"
+    cp "$CONFIG_PATH" "$home/.$PROGRAM_NAME_LOWER/config.ini"
     chown -R "$username":"$username" "$home/.$PROGRAM_NAME_LOWER"
   fi
 done
 
 # Create uninstall script
-cp ./uninstall.sh "$INSTALL_PATH"/
+cp "$UNINSTALL_SCRIPT_PATH" "$INSTALL_PATH"/
 chmod +x "$INSTALL_PATH"/uninstall.sh
 
 # Make executable
-cp ../"$PROGRAM_NAME_LOWER".sh "$INSTALL_PATH"/
-chmod +x "$INSTALL_PATH"/"$PROGRAM_NAME_LOWER".sh
-ln -sf "$INSTALL_PATH"/"$PROGRAM_NAME_LOWER".sh /usr/local/bin/"$PROGRAM_NAME_LOWER"
-chmod +x /usr/local/bin/"$PROGRAM_NAME_LOWER"
+cp "$EXECUTABLE_PATH" "$INSTALL_PATH"/
+chmod +x "${INSTALL_PATH}/${PROGRAM_NAME_LOWER}.sh"
+ln -sf "${INSTALL_PATH}/${PROGRAM_NAME_LOWER}.sh" "/usr/local/bin/${PROGRAM_NAME_LOWER}"
+chmod +x "/usr/local/bin/${PROGRAM_NAME_LOWER}"
 
 # Install dependencies for app
 python$PYTHON_VERSION -m venv "$INSTALL_PATH"/venv
@@ -114,36 +141,38 @@ pip install -e .
 # Update icon cache
 # GNOME
 if [ -x "$(command -v gtk-update-icon-cache)" ]; then
-    echo "Обновление кэша иконок GNOME..."
+    log info "Обновление кэша иконок GNOME..."
     gtk-update-icon-cache -f -t "$ICON_PATH"/
 fi
 
 # KDE
 if [ -x "$(command -v kbuildsycoca5)" ]; then
-    echo "Обновление кэша иконок KDE..."
+    log info "Обновление кэша иконок KDE..."
     kbuildsycoca5 --noincremental
 fi
 
 # XFCE
 if [ -x "$(command -v xfce4-panel)" ]; then
-    echo "Обновление кэша иконок XFCE..."
+    log info "Обновление кэша иконок XFCE..."
     xfce4-panel --restart
 fi
 
 # LXDE
 if [ -x "$(command -v lxpanelctl)" ]; then
-    echo "Обновление кэша иконок LXDE..."
+    log info "Обновление кэша иконок LXDE..."
     lxpanelctl restart
 fi
 
 # MATE
 if [ -x "$(command -v mate-panel)" ]; then
-    echo "Обновление кэша иконок MATE..."
+    log info "Обновление кэша иконок MATE..."
     mate-panel --replace &
 fi
 
-echo "Обновление кэша иконок завершено."
+log info "Обновление кэша иконок завершено."
 
-echo 'Установка успешно завершена.'
-echo 'Приложение было установлено в ' "$INSTALL_PATH"
-echo 'Вы можете удалить приложение выполнив: ' "$INSTALL_PATH"'/uninstall.sh'
+log info 'Установка успешно завершена.'
+log info "Приложение было установлено в $INSTALL_PATH"
+log info "Вы можете удалить приложение выполнив: $INSTALL_PATH/install/linux/uninstall.sh"
+
+trap - EXIT
